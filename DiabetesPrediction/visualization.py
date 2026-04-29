47d98073b0d6436594de4fcc783324fe83c53094
Robert Arsiaga <rarsiaga@trinity.edu>
2026-04-28 20:35:24 -0500

    question 1

--- DiabetesPrediction/visualization.py
+++ DiabetesPrediction/visualization.py
@@ -15,13 +15,20 @@ except:
 # **ADD YOUR CODE HERE**
 
 # print the first 5 rows of the dataset
-
+print(df.head(5))
 
 # display histogram for each variable
+df.hist(figsize=(10, 10))
+plt.show()
 
 
 # statistical summary of dataset
-    
+print(df.describe())
 
 # print the number of missing values for each variable
+print((df["Glucose"] == 0).sum())
+print((df["BloodPressure"] == 0).sum())
+print((df["SkinThickness"] == 0).sum())
+print((df["Insulin"] == 0).sum())
+print((df["BMI"] == 0).sum())
 
