aa5a182b6d168a78ba946328b701bc884988b6c1
Dana Gingrey <dpg2208@hsutx.edu>
2026-05-03 13:11:47 -0500

    preprocess function done

--- DiabetesPrediction/utils.py
+++ DiabetesPrediction/utils.py
@@ -4,8 +4,6 @@ from sklearn.preprocessing import StandardScaler
 from sklearn import preprocessing
 
 def preprocess(df):
-    #Perform mean imputation on the dataset 
-    # (replace the 0 values of the columns with themean of the non-missing values)
     df = df.copy()
     missing_cols = ["Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI"]
 
@@ -16,8 +14,14 @@ def preprocess(df):
             df[col] = df[col].replace(0, col_mean)  # replace 0 with mean
 
     # Step 2 - Print out the number of 0-valued entries for each variable 
-    # to ensure that your imputation worked as intended
     print("\nNumber of 0-valued entries for each variable after imputation:")
     for col in missing_cols:
         if col in df.columns:
             print(f"{col}: {(df[col] == 0).sum()} zero-valued entries")
+
+    # Step 3 - Standardize the dataset (except for target variable - outcome column)
+    feature_cols = df.columns.drop("Outcome")  # exclude the outcome column
+    scaler = StandardScaler()
+    df[feature_cols] = scaler.fit_transform(df[feature_cols]) #scale each feature column
+
+    return df
