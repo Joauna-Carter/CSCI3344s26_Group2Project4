efc77694f70e52dd4cff6d0c9df5376f9d19b825
Dana Gingrey <dpg2208@hsutx.edu>
2026-05-04 15:49:11 -0500

    model created and optimizing still

--- DiabetesPrediction/main.py
+++ DiabetesPrediction/main.py
@@ -29,3 +29,21 @@ y = df["Outcome"].values
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, train_size=0.8, random_state = 16
 )
+
+# Build MLP
+model = Sequential()
+model.add(Dense(32, activation="relu", input_shape=(8,))) #hidden layer 1
+model.add(Dense(16, activation="relu")) # hidden layer 2 
+model.add(Dense(1, activation="sigmoid")) #output layer
+
+# Configure the model for training
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+# Train the model
+model.fit(X_train, y_train, epochs=11, batch_size=16, validation_split=0.1, verbose=2)
+
+# Evaluate model accuracy on the testing data
+loss, accuracy = model.evaluate(X_test, y_test, verbose=0)
+print(f"Test loss: {loss:.4f}")
+print(f"Test accuracy: {accuracy:.4f}")
+
