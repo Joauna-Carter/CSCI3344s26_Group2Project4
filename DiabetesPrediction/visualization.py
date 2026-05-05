b07071a0bf827ad246204d092c5b6ee251893f30
Dana Gingrey <dpg2208@hsutx.edu>
2026-05-05 11:39:11 -0500

    Median and Mode added

--- DiabetesPrediction/visualization.py
+++ DiabetesPrediction/visualization.py
@@ -24,11 +24,16 @@ plt.show()
 
 # statistical summary of dataset
 print("\nStatistical summary of the dataset:")
-print(df.describe())
+pd.set_option('display.max_columns', None)
+summary = df.describe()
+summary.loc['median'] = df.median()
+summary.loc['mode'] = df.mode().iloc[0]
+summary = summary.drop('50%')
+print(summary)
 
 # print the number of missing values for each variable
 print("\nNumber of missing values for each variable:")
-print("Glucose: ", (df["Glucose"] == 0).sum(), "missing out of ", len(df)) #5 missing
+print("Glucose: ", (df["Glucose"] == 0).sum(), "missing out of: ", len(df)) #5 missing
 print("BloodPressure: ", (df["BloodPressure"] == 0).sum(), "missing out of ", len(df)) #35 missing
 print("SkinThickness: ", (df["SkinThickness"] == 0).sum(), "missing out of ", len(df)) #227 missing
 print("Insulin: ", (df["Insulin"] == 0).sum(), "missing out of ", len(df)) #374 missing
